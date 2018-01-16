915352b7a73420deaefc85ac1dbcf96e0fcc5890
regbake <regbake@gmail.com>
2018-01-16 14:36:59 -0800

    finished the clicker bar but it's not dynamic

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
+//dealing with the burger menu
 document.getElementById("nav-toggle").addEventListener ("click", toggleNav);
 function toggleNav() {
         var nav = document.getElementById("navbarMenu");
         var className = nav.getAttribute("class");
-        if(className == "nav-right nav-menu") {
+        if (className == "nav-right nav-menu") {
             nav.className = "nav-right nav-menu is-active";
         } else {
             nav.className = "nav-right nav-menu";
         }
-}
+};
+
+//The part of the portfolio section where you toggle the sections...
+var toggleUl = document.getElementById("toggle-ul").getElementsByTagName("li");
+var pastProj = toggleUl[0].firstChild;
+var currProj = toggleUl[1].firstChild;
+
+pastProj.addEventListener("click", function(){
+  //change this to not selected
+  if (this.classList.contains("selected-title")) {
+    this.classList.remove("selected-title");
+    this.classList.add("not-selected-title");
+
+    //change the other to selected
+    currProj.classList.remove("not-selected-title");
+    currProj.classList.add("selected-title");
+  } else if (this.classList.contains("not-selected-title")){
+    this.classList.remove("not-selected-title");
+    this.classList.add("selected-title");
+
+    //change the other to selected
+    currProj.classList.remove("selected-title");
+    currProj.classList.add("not-selected-title");
+  }
+});
+currProj.addEventListener("click", function(){
+  //change this to not selected
+  if (this.classList.contains("selected-title")) {
+    this.classList.remove("selected-title");
+    this.classList.add("not-selected-title");
+
+    //change the other to selected
+    pastProj.classList.remove("not-selected-title");
+    pastProj.classList.add("selected-title");
+  } else if (this.classList.contains("not-selected-title")){
+    this.classList.remove("not-selected-title");
+    this.classList.add("selected-title");
+
+    //change the other to selected
+    pastProj.classList.remove("selected-title");
+    pastProj.classList.add("not-selected-title");
+  }
+});
