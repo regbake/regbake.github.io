c9628b963d60cb671bf90378e1f3fa49e8d44c8a
Reggie Baker <reggie.baker@gettyimages.com>
2022-07-01 17:00:21 -0700

    more better

--- js/global-header.js
+++ js/global-header.js
@@ -5,11 +5,13 @@ class GlobalHeader extends HTMLElement {
     this.shadow = this.attachShadow({ mode: 'open' });
     this.heading = '';
     this.subheading = '';
+    this.href = '';
   }
 
   connectedCallback() {
     this.heading = this.getAttribute('heading');
     this.subheading = this.getAttribute('subheading');
+    this.href = this.getAttribute('href');
 
     this.render();
   }
@@ -17,8 +19,12 @@ class GlobalHeader extends HTMLElement {
   render() {
     this.shadow.innerHTML = `
       <header part="global-header">
-        <h1>${this.heading}</h1>
-        <p>${this.subheading}</p>
+        <h1 part="header">
+          <a part="anchor" href="${this.href}">
+            ${this.heading}
+          </a>
+        </h1>
+        <p part="sub-header">${this.subheading}</p>
       </header>
     `;
   }
